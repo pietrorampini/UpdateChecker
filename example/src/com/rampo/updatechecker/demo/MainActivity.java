50066dfd7bf7a8d876db1759355bca80534cd40e
Pietro Rampini <rampini.pietro@gmail.com>
2013-08-19 00:03:55 +0200

    - Structure Changes - 2

---
+++ example/src/com/rampo/updatechecker/demo/MainActivity.java
@@ -0,0 +1,26 @@
+package com.rampo.updatechecker.demo;
+
+import com.rampo.updatechecker.UpdateChecker;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+
+public class MainActivity extends FragmentActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		UpdateChecker.CheckForDialog(this);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
