bc537d5bb33bd472bb00e83ff899ae211ec1cea3
Pietro Rampini <rampini.pietro@gmail.com>
2013-08-19 00:14:26 +0200

    - Small Changes

--- example/src/com/rampo/updatechecker/demo/MainActivity.java
+++ example/src/com/rampo/updatechecker/demo/MainActivity.java
@@ -15,12 +15,4 @@ public class MainActivity extends FragmentActivity {
 		
 		UpdateChecker.CheckForDialog(this);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
-
 }
