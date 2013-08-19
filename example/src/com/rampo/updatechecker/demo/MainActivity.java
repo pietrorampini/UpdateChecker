cbca78d7314c016610b0e8a0687490c314efdfa1
Pietro Rampini <rampini.pietro@gmail.com>
2013-08-19 02:56:41 +0200

    New arts

--- example/src/com/rampo/updatechecker/demo/MainActivity.java
+++ example/src/com/rampo/updatechecker/demo/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		UpdateChecker.CheckForNotification(this);
 	}
 }
