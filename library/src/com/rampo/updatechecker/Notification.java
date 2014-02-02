fb13afdec4bee92ac693bca38c1eb490706c6b92
Pietro Rampini <rampini.pietro@gmail.com>
2014-02-02 11:43:22 +0100

    License renewed

--- library/src/com/rampo/updatechecker/Notification.java
+++ library/src/com/rampo/updatechecker/Notification.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Pietro Rampini "Rampo" - Piko Technologies
+ * Copyright (C) 2014 Pietro Rampini - PiKo Technologies
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
--- library/src/com/rampo/updatechecker/UpdateCheckerResult.java
+++ library/src/com/rampo/updatechecker/UpdateCheckerResult.java
@@ -1,3 +1,15 @@
+/*
+ * Copyright (C) 2014 Pietro Rampini - PiKo Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * limitations under the License.
+ */
 package com.rampo.updatechecker;
 
 /**
