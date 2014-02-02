e4206fa9d8a30c96d2f8f86fabb177d758b54174
Pietro Rampini <rampini.pietro@gmail.com>
2014-02-02 12:03:23 +0100

    Added Amazon App Store support

--- library/src/com/rampo/updatechecker/ASyncCheck.java
+++ library/src/com/rampo/updatechecker/ASyncCheck.java
@@ -35,60 +35,81 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 
 /**
- * Heart of the library. Check if an update is available for download parsing the desktop Play Store page of the app
+ * Heart of the library. Check if an update is available for download parsing the desktop Play Store/Amazon App Store page of your app.
+ *
+ * @author Pietro Rampini (rampini.pietro@gmail.com)
  */
 class ASyncCheck extends AsyncTask<String, Integer, Integer> {
-    private static final String ROOT_PLAY_STORE_WEB = "https://play.google.com/store/apps/details?id=";
-    private static final String HTML_TAGS_TO_GET_RIGHT_LINE = "</script> </div> <div class=\"details-wrapper\">";
-    private static final String HTML_TAGS_TO_GET_RIGHT_POSITION = "itemprop=\"softwareVersion\"> ";
-    private static final String HTML_TAGS_TO_REMOVE_USELESS_CONTENT = "  </div> </div>";
+    private static final String PLAY_STORE_ROOT_WEB = "https://play.google.com/store/apps/details?id=";
+    private static final String PLAY_STORE_HTML_TAGS_TO_GET_RIGHT_LINE = "</script> </div> <div class=\"details-wrapper\">";
+    private static final String PLAY_STORE_HTML_TAGS_TO_GET_RIGHT_POSITION = "itemprop=\"softwareVersion\"> ";
+    private static final String PLAY_STORE_HTML_TAGS_TO_REMOVE_USELESS_CONTENT = "  </div> </div>";
+    private static final String PLAY_STORE_PACKAGE_NOT_PUBLISHED_IDENTIFIER = "We're sorry, the requested URL was not found on this server.";
+
+    private static final String AMAZON_STORE_ROOT_WEB = "http://www.amazon.com/gp/mas/dl/android?p=";
+    private static final String AMAZON_STORE_HTML_TAGS_TO_GET_RIGHT_LINE = "<li><strong>Version:</strong>";
+    private static final String AMAZON_STORE_PACKAGE_NOT_PUBLISHED_IDENTIFIER = "<title>Amazon.com: Apps for Android</title>";
+
     private static final String LOG_TAG = "UpdateChecker";
     private static final int VERSION_DOWNLOADABLE_FOUND = 0;
     private static final int MULTIPLE_APKS_PUBLISHED = 1;
     private static final int NETWORK_ERROR = 2;
+    private static final int PACKAGE_NOT_PUBLISHED = 3;
 
-    Store store;
-    Context context;
-    ASyncCheckResult resultInterface;
-    String versionDownloadable;
+    Store mStore;
+    Context mContext;
+    ASyncCheckResult mResultInterface;
+    String mVersionDownloadable;
 
     ASyncCheck(Store store, ASyncCheckResult resultInterface, Context activity) {
-        this.store = store;
-        this.resultInterface = resultInterface;
-        this.context = activity;
+        this.mStore = store;
+        this.mResultInterface = resultInterface;
+        this.mContext = activity;
     }
 
     @Override
     protected Integer doInBackground(String... notused) {
-        if (isNetworkAvailable(context)) {
+        if (isNetworkAvailable(mContext)) {
             try {
-                Log.d(LOG_TAG, "zcas");
                 HttpParams params = new BasicHttpParams();
                 HttpConnectionParams.setConnectionTimeout(params, 4000);
                 HttpConnectionParams.setSoTimeout(params, 5000);
                 HttpClient client = new DefaultHttpClient(params);
-                if (store == Store.GOOGLE_PLAY) {
-                    HttpGet request = new HttpGet(ROOT_PLAY_STORE_WEB + context.getPackageName()); // Set the right Play Store page by getting package name.
+                if (mStore == Store.GOOGLE_PLAY) {
+                    HttpGet request = new HttpGet(PLAY_STORE_ROOT_WEB + mContext.getPackageName()); // Set the right Play Store page by getting package name.
                     HttpResponse response = client.execute(request);
                     InputStream is = response.getEntity().getContent();
                     BufferedReader reader = new BufferedReader(new InputStreamReader(is));
                     String line;
                     while ((line = reader.readLine()) != null) {
-                        if (line.contains(HTML_TAGS_TO_GET_RIGHT_LINE)) { // Obtain HTML line contaning version available in Play Store
-                            String containingVersion = line.substring(line.lastIndexOf(HTML_TAGS_TO_GET_RIGHT_POSITION) + 28);  // Get the String starting with version available + Other HTML tags
-                            String[] removingUnusefulTags = containingVersion.split(HTML_TAGS_TO_REMOVE_USELESS_CONTENT); // Remove unseful HTML tags
-                            versionDownloadable = removingUnusefulTags[0]; // Obtain version available
+                        if (line.contains(PLAY_STORE_HTML_TAGS_TO_GET_RIGHT_LINE)) { // Obtain HTML line contaning version available in Play Store
+                            String containingVersion = line.substring(line.lastIndexOf(PLAY_STORE_HTML_TAGS_TO_GET_RIGHT_POSITION) + 28);  // Get the String starting with version available + Other HTML tags
+                            String[] removingUnusefulTags = containingVersion.split(PLAY_STORE_HTML_TAGS_TO_REMOVE_USELESS_CONTENT); // Remove useless HTML tags
+                            mVersionDownloadable = removingUnusefulTags[0]; // Obtain version available
+                        } else if (line.contains(PLAY_STORE_PACKAGE_NOT_PUBLISHED_IDENTIFIER)) { // This packages has not been found in Play Store
+                            return PACKAGE_NOT_PUBLISHED;
                         }
                     }
-
-                } else if (store == Store.AMAZON) {
-
-                }
-
-                if (containsNumber(versionDownloadable)) {
-                    return VERSION_DOWNLOADABLE_FOUND;
-                } else {
-                    return MULTIPLE_APKS_PUBLISHED;
+                    if (containsNumber(mVersionDownloadable)) {
+                        return VERSION_DOWNLOADABLE_FOUND;
+                    } else {
+                        return MULTIPLE_APKS_PUBLISHED;
+                    }
+                } else if (mStore == Store.AMAZON) {
+                    HttpGet request = new HttpGet(AMAZON_STORE_ROOT_WEB + mContext.getPackageName()); // Set the right Amazon App Store page by getting package name.
+                    HttpResponse response = client.execute(request);
+                    InputStream is = response.getEntity().getContent();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        if (line.contains(AMAZON_STORE_HTML_TAGS_TO_GET_RIGHT_LINE)) { // Obtain HTML line contaning version available in Amazon App Store
+                            String versionDownloadableWithTags = line.substring(38); // Get the String starting with version available + Other HTML tags
+                            mVersionDownloadable = versionDownloadableWithTags.substring(0, versionDownloadableWithTags.length() - 5); // Remove useless HTML tags
+                            return VERSION_DOWNLOADABLE_FOUND;
+                        } else if (line.contains(AMAZON_STORE_PACKAGE_NOT_PUBLISHED_IDENTIFIER)) { // This packages has not been found in Amazon App Store
+                            return PACKAGE_NOT_PUBLISHED;
+                        }
+                    }
                 }
             } catch (IOException connectionError) {
                 logConnectionError();
@@ -97,6 +118,7 @@ class ASyncCheck extends AsyncTask<String, Integer, Integer> {
         } else {
             return NETWORK_ERROR;
         }
+        return null;
     }
 
     /**
@@ -107,20 +129,22 @@ class ASyncCheck extends AsyncTask<String, Integer, Integer> {
     @Override
     protected void onPostExecute(Integer result) {
         if (result == VERSION_DOWNLOADABLE_FOUND) {
-            resultInterface.versionDownloadableFound(versionDownloadable);
-            Log.d(LOG_TAG, "teet");
+            mResultInterface.versionDownloadableFound(mVersionDownloadable);
         } else if (result == NETWORK_ERROR) {
-            resultInterface.networkError();
-            Log.d(LOG_TAG, "a");
+            mResultInterface.networkError();
+            logConnectionError();
         } else if (result == MULTIPLE_APKS_PUBLISHED) {
-            resultInterface.multipleApksPublished();
-            Log.d(LOG_TAG, "b");
+            mResultInterface.multipleApksPublished();
+            Log.e(LOG_TAG, "Multiple APKs published ");
+        } else if (result == PACKAGE_NOT_PUBLISHED) {
+            mResultInterface.appUnpublished();
+            Log.e(LOG_TAG, "App unpublished");
         }
     }
 
     /**
-     * Since the library check from the Desktop Web Page of the app the Current Version, if there are different apks for the app,
-     * the Play Store will shown Varies depending on the device, so the Library can't compare it to versionName installed.
+     * Since the library check from the Desktop Web Page of the app the "Current Version" field, if there are different apks for the app,
+     * the Play Store will shown "Varies depending on the device", so the library can't compare it to versionName installed.
      *
      * @see <a href="https://github.com/rampo/UpdateChecker/issues/1">Issue #1</a>
      */
--- library/src/com/rampo/updatechecker/Store.java
+++ library/src/com/rampo/updatechecker/Store.java
@@ -1,7 +1,24 @@
+/*
+ * Copyright (C) 2014 Pietro Rampini - PiKo Technologies
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.rampo.updatechecker;
 
 /**
- * Created by Rampo on 20/10/13.
+ * The store where your app is published. Settable with setStore(Store). Default is Google Play.
+ * @see com.rampo.updatechecker.UpdateChecker#setStore(Store)
+ * @author Pietro Rampini (rampini.pietro@gmail.com)
  */
 public class Store {
     public static final Store GOOGLE_PLAY = new Store(0);
