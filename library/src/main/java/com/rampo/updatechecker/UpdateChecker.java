34245f6d58eb4d0128852c2795e4ef05d4efdb87
Rampo <rampini.pietro@gmail.com>
2014-07-06 20:49:27 +0200

    Removed useless code from UpdateChecker.java

--- library/src/main/java/com/rampo/updatechecker/UpdateChecker.java
+++ library/src/main/java/com/rampo/updatechecker/UpdateChecker.java
@@ -17,7 +17,6 @@ package com.rampo.updatechecker;
 
 import android.app.Activity;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
 import android.support.v4.app.FragmentActivity;
 
 /**
@@ -45,7 +44,6 @@ public class UpdateChecker implements ASyncCheckResult, UpdateCheckerResult {
     static UpdateCheckerResult mLibraryResultCallaback;
     static ASyncCheckResult mCheckResultCallback;
     static boolean mCustomImplementation;
-    static boolean mCompareNumerically = false; //for beta testers
 
     public UpdateChecker(Activity activity) {
         mActivity = activity;
@@ -67,10 +65,6 @@ public class UpdateChecker implements ASyncCheckResult, UpdateCheckerResult {
         mCheckResultCallback = this;
         mLibraryResultCallaback = updateCheckerResult;
         mCustomImplementation = true;
-    }
-
-    public static void setCompareNumerically(boolean compareNumerically) {
-        mCompareNumerically = compareNumerically;
     }
 
     /**
@@ -147,69 +141,6 @@ public class UpdateChecker implements ASyncCheckResult, UpdateCheckerResult {
             mLibraryResultCallaback.upToDate(versionDownloadable);
         }
 
-    }
-
-    /**
-     * Compare the string versionDownloadable to the version installed of the app.
-     *
-     * @param versionDownloadable String to compare to the version installed of the app.
-     */
-    private boolean versionDownloadableIsDifferent(String versionDownloadable) {
-        try {
-            return !versionDownloadable.equals(mActivity.getPackageManager().getPackageInfo(mActivity.getPackageName(), 0).versionName);
-        } catch (PackageManager.NameNotFoundException ignored) {
-        }
-        return false;
-    }
-
-    /**
-     * Compare the string versionDownloadable to the version installed of the app.
-     *
-     * @param versionDownloadable String to compare to the version installed of the app.
-     */
-    private boolean versionDownloadableIsGreat(String versionDownloadable) {
-        try {
-            String versionInstalled = mActivity.getPackageManager().getPackageInfo(mActivity.getPackageName(), 0).versionName;
-            if (versionInstalled != null && versionDownloadable != null)
-                return versionCompare(versionDownloadable, versionInstalled) > 0;
-        } catch (PackageManager.NameNotFoundException ignored) {
-        }
-        return false;
-    }
-
-    /**
-     * https://stackoverflow.com/questions/6701948/efficient-way-to-compare-version-strings-in-java
-     * <p/>
-     * Compares two version strings.
-     * <p/>
-     * Use this instead of String.compareTo() for a non-lexicographical
-     * comparison that works for version strings. e.g. "1.10".compareTo("1.6").
-     *
-     * @param str1 a string of ordinal numbers separated by decimal points.
-     * @param str2 a string of ordinal numbers separated by decimal points.
-     * @return The result is a negative integer if str1 is _numerically_ less than str2.
-     * The result is a positive integer if str1 is _numerically_ greater than str2.
-     * The result is zero if the strings are _numerically_ equal.
-     * @note It does not work if "1.10" is supposed to be equal to "1.10.0".
-     */
-    public Integer versionCompare(String str1, String str2) {
-        String[] vals1 = str1.split("\\.");
-        String[] vals2 = str2.split("\\.");
-        int i = 0;
-        // set index to first non-equal ordinal or length of shortest version string
-        while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i])) {
-            i++;
-        }
-        // compare first non-equal ordinal number
-        if (i < vals1.length && i < vals2.length) {
-            int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
-            return Integer.signum(diff);
-        }
-        // the strings are equal or one string is a substring of the other
-        // e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
-        else {
-            return Integer.signum(vals1.length - vals2.length);
-        }
     }
 
     /**
